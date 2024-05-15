f57e0490754b98db669b9536bcaf1e7aeaa10831
RedBeardDev <julien.deloyers@gmail.com>
2024-05-15 22:56:28 +0200

    Disable frontend caching for index page

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,13 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
-using WebEssentials.AspNetCore.OutputCaching;
 
 namespace VsixGallery.Pages
 {
@@ -27,7 +22,6 @@ namespace VsixGallery.Pages
 
 		public void OnGet([FromQuery] int page = 1)
 		{
-			HttpContext.EnableOutputCaching(TimeSpan.FromDays(7), fileProvider: _helper.FileProvider, fileDependencies: "*", varyByParam: "page");
 			IEnumerable<Package> packages = _helper.PackageCache.Where(p => !p.Unlisted);
 
 			int skip = (page - 1) * _pageSize;
